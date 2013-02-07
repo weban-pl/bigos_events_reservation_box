557a3fabfc82663161191de7ad04f43e9ae62e47
kremenso <kremenso@o2.pl>
2013-02-07 19:59:46 +0100

    Update settings

--- lib/generators/bigos_events_reservation_box/install_generator.rb
+++ lib/generators/bigos_events_reservation_box/install_generator.rb
@@ -19,16 +19,16 @@ module BigosEventsReservationBox
       end
 
       def create_settings
-        Setting["GS.use_widget_mailer_settings"] = false
-        Setting["GS.mailer_port"] = 587
-        Setting["GS.mailer_domain"] = "yourdomain.dev"
-        Setting["GS.mailer_address_name"] = "smtp.gmail.com"
-        Setting["GS.mailer_user_name"] = "from@yourdomain.dev"
-        Setting["GS.mailer_password"] = "Super-Secure-Password"
-        Setting["#{BigosEventsReservationBox.name}.recipient"] = "Your email address"
-        Setting["#{BigosEventsReservationBox.name}.invalid_fields_error"] = "Invalid Fields"
-        Setting["#{BigosEventsReservationBox.name}.success_message"] = "Message sent successfully"
-        Setting["#{BigosEventsReservationBox.name}.redirect_path"] = ""
+        Setting["GS.use_widget_mailer_settings"] = false if Setting.find_by_var("GS.use_widget_mailer_settings").blank?
+        Setting["GS.mailer_port"] = 587 if Setting.find_by_var("GS.mailer_port").blank?
+        Setting["GS.mailer_domain"] = "yourdomain.dev" if Setting.find_by_var("GS.mailer_domain").blank?
+        Setting["GS.mailer_address_name"] = "smtp.gmail.com" if Setting.find_by_var("GS.mailer_address_name").blank?
+        Setting["GS.mailer_user_name"] = "from@yourdomain.dev" if Setting.find_by_var("GS.mailer_user_name").blank?
+        Setting["GS.mailer_password"] = "Super-Secure-Password" if Setting.find_by_var("GS.mailer_password").blank?
+        Setting["#{BigosEventsReservationBox.name}.recipient"] = "Your email address" if Setting.find_by_var("#{BigosEventsReservationBox.name}.recipient").blank?
+        Setting["#{BigosEventsReservationBox.name}.invalid_fields_error"] = "Invalid Fields" if Setting.find_by_var("#{BigosEventsReservationBox.name}.invalid_fields_error").blank?
+        Setting["#{BigosEventsReservationBox.name}.success_message"] = "Message sent successfully" if Setting.find_by_var("#{BigosEventsReservationBox.name}.success_message").blank?
+        Setting["#{BigosEventsReservationBox.name}.redirect_path"] = "" if Setting.find_by_var("#{BigosEventsReservationBox.name}.redirect_path").blank?
         im = BigosApp::InstalledModule.find_or_create_by_name(BigosEventsReservationBox.name)
         im.page_element =  BigosApp::BigosEventsReservationBoxElement.name
         im.save
