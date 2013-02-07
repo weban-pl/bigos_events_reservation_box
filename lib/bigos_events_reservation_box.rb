f729f7a60f44347d9df4c96c9279aeb0698734f7
kremenso <kremenso@o2.pl>
2013-02-07 18:43:43 +0100

    module setup

--- lib/bigos_events_reservation_box.rb
+++ lib/bigos_events_reservation_box.rb
@@ -1,4 +1,21 @@
 require "bigos_events_reservation_box/engine"
 
 module BigosEventsReservationBox
+  module Controllers
+    autoload :Helpers, 'bigos_events_reservation_box/controllers/helpers'
+  end
+
+  def self.setup
+    include_helpers
+    yield self
+  end
+
+  def self.include_helpers
+    ActiveSupport.on_load(:action_controller) do
+    end
+
+    ActiveSupport.on_load(:action_view) do
+      include BigosEventsReservationBox::Controllers::Helpers
+    end
+  end
 end
